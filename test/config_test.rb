1286d62cdb8f40f77b4d4e7c3385b49a33d50777
Josh Mills <jmills@covermymeds.com>
2016-05-24 09:55:15 -0400

    Config class now has tests :heart:

---
+++ test/config_test.rb
@@ -0,0 +1,20 @@
+require_relative 'test_helper'
+require 'config'
+
+class ConfigTest < Minitest::Test
+  def setup
+    ENV['CONFIG_PATH'] = 'test/fixtures/config.test'
+  end
+
+  def test_base_path
+    assert_equal('tmp', Config.base_path)
+  end
+
+  def test_yaml_path
+    assert_equal('tmp/_yaml_', Config.yaml_path)
+  end
+
+  def test_notes_path
+    assert_equal('tmp/notes', Config.notes_path)
+  end
+end
