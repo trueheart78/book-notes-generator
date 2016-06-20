0e54d45af547aff3b8920bce08fb3104039906fd
Josh Mills <josh@trueheart78.com>
2016-06-20 09:45:33 -0400

    More support for the Pathnames

--- test/config_test.rb
+++ test/config_test.rb
@@ -45,13 +45,13 @@ class ConfigTest < Minitest::Test
   end
 
   def test_compose_notes_dir
-    assert_equal 'notes/book-dir', subject.compose_notes_dir('book-dir')
+    assert_equal Pathname.new('notes/book-dir'), subject.compose_notes_dir('book-dir')
   end
 
   def test_compose_notes_dir_when_nil
     config_path = fixture_config_path empty_notes_config_file
     empty_notes_config = Config.new config_path: config_path
-    assert_equal 'book-dir', empty_notes_config.compose_notes_dir('book-dir')
+    assert_equal Pathname.new('book-dir'), empty_notes_config.compose_notes_dir('book-dir')
   end
 
   def test_relative_path
