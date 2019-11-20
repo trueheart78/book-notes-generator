d3860d9c279c2a8a0d0466194ca249e2cb74c6ac
Josh Mills <josh@trueheart78.com>
2019-11-20 11:55:19 -0500

    Remove byebug

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,11 +3,9 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 ENV['NODE_ENV'] = 'test'
 ENV['CONFIG_PATH'] = 'test/fixtures/config.test'
 
-require 'byebug'
 require 'pry'
 require 'fileutils'
 require 'minitest/autorun'
-require 'byebug'
 require 'file_system_test_mixin'
 require 'config'
 require 'options'
